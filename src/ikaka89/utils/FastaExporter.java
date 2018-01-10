596c81a2407200e005049653beb96afb24358fb3
akakumanu <akakumanu@localhost>
2018-01-10 14:08:59 -0500

    making few changes to fasta exporter

--- src/ikaka89/utils/FastaExporter.java
+++ src/ikaka89/utils/FastaExporter.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.seqcode.data.io.RegionFileUtilities;
 import org.seqcode.genome.Genome;
+import org.seqcode.genome.GenomeConfig;
 import org.seqcode.genome.Species;
 import org.seqcode.genome.location.Point;
 import org.seqcode.genome.location.Region;
@@ -27,28 +28,21 @@ public class FastaExporter {
 	
 	
 	public List<Region> regs;
-	public Genome gen;
-	public SequenceGenerator<Region> seqgen = new SequenceGenerator<Region>();;
+	//public SequenceGenerator<Region> seqgen = new SequenceGenerator<Region>();;
 	public List<String> fasta = new ArrayList<String>();
 	public List<String> names = new ArrayList<String>();
 	public List<String> strand = new ArrayList<String>();
 	
 	
-	public FastaExporter(int win, List<Region> rs, Genome gen) {
+	public FastaExporter(int win, List<Region> rs) {
 		regs = new ArrayList<Region>();
 		regs.addAll(rs);
-		this.gen = gen;
-		
 	}
 	
-	public void execute(boolean cache, String seqPathName){
-		if(cache){
-			seqgen.useCache(cache);
-			seqgen.setGenomePath(seqPathName);
-		}
+	public void execute(GenomeConfig gcon, String seqPathName){
 		
 		for(int rind=0; rind<regs.size(); rind++){
-			String fa = seqgen.execute(regs.get(rind)).toUpperCase();
+			String fa = gcon.getSequenceGenerator().execute(regs.get(rind)).toUpperCase();
 			if(strand.size()>0 && strand.get(rind).equals("-")){
 				fasta.add(SequenceUtils.reverseComplement(fa));
 			}else{
@@ -74,33 +68,19 @@ public class FastaExporter {
 	public void setStrands(List<String>sts){strand.addAll(sts);}
 	
 	public static void main(String[] args) throws IOException{
-		try{
+		
 			ArgParser ap =  new ArgParser(args);
+			GenomeConfig gcon = new GenomeConfig(args);
+			
 			int window = Args.parseInteger(args, "win", -1);
-			boolean cache = ap.hasKey("cache");
-			String seqPathName = "";
-			if(cache){
-				seqPathName = Args.parseString(args, "seq", "");
-			}
 			
-			if(!ap.hasKey("locations") && !ap.hasKey("regions")){
-				System.err.println("Provide either a location or a regions file !!");
+			
+			String seqPathName = Args.parseString(args, "seq", "");
+		
+			
+			if(!ap.hasKey("points")){
+				System.err.println("Provide either a points or a regions file!!");
 				System.exit(1);
-			}
-			
-			Genome gen;
-			if(ap.hasKey("species") || ap.hasKey("genome") || ap.hasKey("gen")){
-				Pair<Species, Genome> orggen = Args.parseGenome(args);
-				gen = orggen.cdr();
-			} 
-			else{
-				if(ap.hasKey("geneinfo") || ap.hasKey("g")){
-					String infofilename = ap.hasKey("geneinfo") ? ap.getKeyValue("geneinfo") : ap.getKeyValue("g"); 
-					gen = new Genome("Genome",new File(infofilename),true);
-				}
-				else{
-					gen = null;
-				}
 			}
 			
 			boolean hasnames = ap.hasKey("hasnames");
@@ -123,23 +103,21 @@ public class FastaExporter {
 			List<Region> search_locs = new ArrayList<Region>();
 			if(isStranded){
 				List<StrandedRegion> srs  = new ArrayList<StrandedRegion>();
-				srs.addAll(RegionFileUtilities.loadStrandedRegionsFromMotifFile(gen, ap.getKeyValue("locations"), window));
+				srs.addAll(RegionFileUtilities.loadStrandedRegionsFromMotifFile(gcon.getGenome(), ap.getKeyValue("points"), window));
 				for(StrandedRegion sr:  srs){
-					search_locs.add(new Region(gen, sr.getChrom(), sr.getStart(), sr.getEnd()));
+					search_locs.add(new Region(gcon.getGenome(), sr.getChrom(), sr.getStart(), sr.getEnd()));
 				}
 			}else{
-				search_locs.addAll(RegionFileUtilities.loadRegionsFromPeakFile(gen, ap.getKeyValue("locations"), window));
+				search_locs.addAll(RegionFileUtilities.loadRegionsFromPeakFile(gcon.getGenome(), ap.getKeyValue("points"), window));
 			}
 			
-			FastaExporter exporter = new FastaExporter(window, search_locs, gen);
+			FastaExporter exporter = new FastaExporter(window, search_locs);
 			if(hasnames)
 				exporter.setNames(ns);
 			if(isStranded)
 				exporter.setStrands(strs);
-			exporter.execute(cache, seqPathName);
-		}catch(NotFoundException e){
-			e.printStackTrace();
-		} 
+			exporter.execute(gcon,seqPathName);
+ 
 	}
 
 }
